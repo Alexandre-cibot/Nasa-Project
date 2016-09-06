b19a6e0fb5a029a5cac269f446ece19c04119a42
Alexandre Cibot <alexandre.cibot@gmail.com>
2016-09-06 10:13:00 +0200

    Minor textual correction in the footer

--- src/js/modules/Footer.js
+++ src/js/modules/Footer.js
@@ -7,7 +7,7 @@ const Footer = function () {
 					            <div className="row">
 					              <div className="col l6 s12">
 					                <h5 className="white-text">Why this Website</h5>
-					                <p className="grey-text text-lighten-4">I've made this website to improve my knowledge using those <b>Technologies / Frameworks / Libraries</b> : <i>Html - Css - Javascript - React - NodeJS.</i> <br /> Also to work with an API. I use the the NASA Open API, available here <a href="https://api.nasa.gov/index.html">https://api.nasa.gov/index.html</a>to find Photos and descriptions trasnmited by the Rover.</p>
+					                <p className="grey-text text-lighten-4">I've made this website to improve my knowledge using those <b>Technologies / Frameworks / Libraries</b> : <i>Html - Css - Javascript - React - NodeJS.</i> <br /> Also to work with an API. I use the the NASA Open API, available here <a href="https://api.nasa.gov/index.html">https://api.nasa.gov</a> to find Photos and descriptions trasnmited by the Rover.</p>
 					              </div>
 					              <div className="col l4 offset-l2 s12">
 					                <h5 className="white-text">Want to see the code ? </h5>
